f41166f4b8f4a5fb71ce0f968374c5dad9b119c8
thai <thai30082k2@gmail.com>
2021-10-04 23:01:33 +0700

    Nộp Bài

---
+++ src/com/thainm2005110004/Tuan03/NhanVien.java
@@ -0,0 +1,22 @@
+package com.thainm2005110004.Tuan03;
+public class NhanVien {
+    String TenNhanVien;
+    double Luong;
+    String diachi;
+    String bophanlamviec;
+    String NgaySinh;
+    NhanVien(){
+    }
+    NhanVien(String ten, double l, String d, String bophan, String sn){
+        TenNhanVien = ten;
+        Luong = l;
+        diachi = d;
+        bophanlamviec = bophan;
+        NgaySinh = sn;
+    }
+    void inThongtin(){
+        
+    }
+
+    
+}
